fd621d9ea55e08d2ec24c07fc1a81d089ef9394b
João Nunes <lockenunes@gmail.com>
2019-08-31 12:09:52 -0300

    6.3 OAuth2 e Password Flow

--- src/main/java/com/algaworks/algamoneyapi/config/SecurityConfig.java
+++ src/main/java/com/algaworks/algamoneyapi/config/SecurityConfig.java
@@ -1,30 +1,18 @@
 package com.algaworks.algamoneyapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //{noop} is required in Spring 5
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}admin").roles("ROLE");
-    }
 
+    // https://stackoverflow.com/questions/52243774/consider-defining-a-bean-of-type-org-springframework-security-authentication-au/52244758
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                    .antMatchers("/categorias").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .httpBasic().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .csrf().disable();
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
